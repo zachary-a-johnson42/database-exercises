1407ab4b38cf8c97e3459299c552f428a59dc993
Zachary Johnson <zachary.a.johnson42@gmail.com>
2022-08-25 16:42:46 -0500

    added comments to all code

--- update_exercises.sql
+++ update_exercises.sql
@@ -1,20 +1,22 @@
+--Selecting whole table
 SELECT * FROM albums;
-
+--Updating all sales
 UPDATE albums
 SET sales = sales * 10;
 
+--Testing select
 SELECT * FROM albums
     WHERE release_date < 1980;
-
+--Updating all albums released before 1980
 UPDATE albums
 SET release_date = release_date - 100
     WHERE release_date < 1980;
-
+--Testing Select
 SELECT * FROM albums
     WHERE artist_name = 'Michael Jackson';
-
+--Updating name to Peter Jackson
 UPDATE albums
 SET artist_name = 'Peter Jackson'
     WHERE artist_name = 'Michael Jackson';
-
+--Query to make sure all updates worked
 SELECT * FROM albums;
