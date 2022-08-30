c75e20ebf2b16f0f926abb56e9dd6998c3473283
Zachary Johnson <zachary.a.johnson42@gmail.com>
2022-08-30 09:43:21 -0500

    completed SQL Where exercises.

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,21 +1,20 @@
---Selecting all employees with specified first names
+--Selecting all employees with specified first names 709 rows
 SELECT * FROM employees
-    WHERE first_name = 'Irina'
+    WHERE first_name = 'Irena'
         OR first_name = 'Vidya'
             OR first_name = 'Maya';
 
---Selecting employees whose last names start with E.
+--Selecting employees whose last names start with E. 7,330 rows.
 SELECT * FROM employees
     WHERE last_name LIKE 'E%';
 
---Selecting employees with 'q' in their last name
+--Selecting employees with 'q' in their last name 1,873 rows.
 SELECT * FROM employees
     WHERE last_name LIKE '%q%';
 
---Select all employees with specific name that are also male
-
+--Select all employees with specific name that are also male 441 rows.
 SELECT * FROM employees
-    WHERE first_name = 'Irina'
+    WHERE first_name = 'Irena'
             AND gender = 'M'
         OR first_name = 'Vidya'
             AND gender = 'M'
@@ -24,7 +23,15 @@ SELECT * FROM employees
 
 --Find all employees whose last name starts or ends with 'E' — 30,723 rows.
 SELECT * FROM employees
+    WHERE last_name LIKE "E%"
+        OR RIGHT(last_name, 1) = 'e';
 
 --Duplicate the previous query and update it to find all employees whose last name starts and ends with 'E' — 899 rows
+SELECT * FROM employees
+WHERE last_name LIKE "E%"
+   AND RIGHT(last_name, 1) = 'e';
 
 --Find all employees with a 'q' in their last name but not 'qu' — 547 rows.
+SELECT * FROM employees
+    WHERE last_name LIKE '%q%'
+        AND last_name NOT LIKE '%qu%';
